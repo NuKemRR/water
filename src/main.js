7ff2d545715cd0f39503f235a23d7a8218c8df7c
NuKemRRR <114313596+NuKemRR@users.noreply.github.com>
2026-03-09 20:40:37 -0100

    Test mesh

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import Stats from 'three/examples/jsm/libs/stats.module.js'
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import Water from './Water.js'
 import DebugUI from "./DebugUI.js";
 import { HDRLoader } from 'three/addons/loaders/HDRLoader.js';
@@ -40,40 +41,9 @@ new DebugUI();
 const timer = new THREE.Timer();
 timer.connect(document);
 
-const geo = new THREE.BoxGeometry(1, 1, 1);
-const mat = new THREE.MeshStandardMaterial({color: 0x202030, roughness: 1.0, metalness: 1.0})
-const matFloor = new THREE.MeshStandardMaterial({color: 0x707070, roughness: 1.0, metalness: 1.0})
-const height = -25;
-const yScale = 60;
-
-const mesh0 = new THREE.Mesh(geo, mat);
-mesh0.scale.set(20, yScale, 150)
-mesh0.position.set(80, height, 0);
-
-const mesh1 = new THREE.Mesh(geo, mat);
-mesh1.scale.set(20, yScale, 150)
-mesh1.position.set(-80, height, 0);
-
-const mesh2 = new THREE.Mesh(geo, mat);
-mesh2.scale.set(150, yScale, 20)
-mesh2.position.set(0, height, 80);
-
-const mesh3 = new THREE.Mesh(geo, mat);
-mesh3.scale.set(150, yScale, 20)
-mesh3.position.set(0, height, -80);
-
-const mesh4 = new THREE.Mesh(geo, matFloor);
-mesh4.scale.set(1000, 1, 1000)
-mesh4.position.set(0, -50, 0);
-
 scene.add(water.getMesh());
 scene.add(sun);
 scene.add(ambientLight);
-scene.add(mesh0);
-scene.add(mesh1);
-scene.add(mesh2);
-scene.add(mesh3);
-scene.add(mesh4);
 
 const hdrLoader = new HDRLoader();
 
@@ -83,6 +53,25 @@ hdrLoader.load('public/skybox.hdr', (texture) => {
     scene.environment = texture;
     renderer.toneMappingExposure = 1.0;
 });
+
+const gltfLoader = new GLTFLoader();
+gltfLoader.load(
+    'public/terrain_mesh_test_water.glb',
+    (gltf) => {
+        const model = gltf.scene;
+        model.scale.set(13, 13, 13);
+        model.position.set(0, 5, 0);
+        scene.add(model);
+
+        console.log("Model loaded successfully!", gltf);
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    (error) => {
+        console.error('An error happened', error);
+    }
+);
 
 function update() {
     stats.begin();
